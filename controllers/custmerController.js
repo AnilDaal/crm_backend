938049d300cbbcee0930e6b8bd2cbab61698411d
Anil Daal <anildaal7@gmail.com>
2023-02-03 13:03:48 +0530

    Update custmerController.js

--- controllers/custmerController.js
+++ controllers/custmerController.js
@@ -2,7 +2,9 @@ import Customer from"../models/customerModel.js"
 const addCustomer = async function(req,res){
     const {name,email,date,status,phone,country,address,isAdmin} = req.body
     if(!name || !email || !date || !status || !phone || !country || !address)
+    {
     return res.status(401).json({message:"Please fill mandatory field"})
+    }
     try {
         const addCust = await Customer.create({
             name,
@@ -23,7 +25,9 @@ const getCustomer = async function(req,res){
     try {
         const getCust = await Customer.find()
         if(!getCust)
+        {
         return res.status(201).json({message:"please add customers"})
+        }
         res.status(201).json(getCust)
     } catch (error) {
         res.status(501).json({message:error})
@@ -34,7 +38,9 @@ const getSingleCustomer= async function(req,res){
         const customerId = req.params.customerId
         const getSingleCust = await Customer.findById(customerId)
         if(!getSingleCust)
+        {
         return res.status(401).json({message:"please enter a valid customer"})
+        }
         res.status(201).json(getSingleCust)
     } catch (error) {
         res.status(501).json({message:error})
@@ -44,7 +50,9 @@ const getSingleCustomer= async function(req,res){
 const updateCustomer = async(req,res)=>{
     const {name,email,date,status,phone,country,address} = req.body
     if(!name || !email || !date || !status || !phone || !country || !address || !isAdmin)
+    {
     return res.status(401).json({message:"Please fill mandatory field"})
+    }
     try {
         const customerId = req.params.customerId
         const updateCust = await Customer.findByIdAndUpdate(customerId,{$set:{
@@ -67,7 +75,9 @@ const deleteCustomer = async(req,res)=>{
         const customerId = req.params.customerId
         const deleteCust = await Customer.findByIdAndDelete(customerId)
         if(!deleteCust)
+        {
         return res.status(401).json({message:"please enter a valid customer"})
+        }
         res.status(201).json(deleteCust)
     } catch (error) {
         res.status(501).json({message:error})
